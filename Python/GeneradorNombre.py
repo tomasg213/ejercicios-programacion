150a7bdc2553a4f07dcd9efdacc09e1c0c5a7119
Tomas <tomasg1337@gmail.com>
2026-03-14 22:30:25 -0400

    Generador Nombre

---
+++ Python/GeneradorNombre.py
@@ -0,0 +1,8 @@
+#Crea un programa que pregunte al usuario el nombre de su mascota y el nombre de su ciudad
+# y luego los combine para darle un "Nombre de Estrella de Rock".
+#Reto: Asegúrate de que haya un espacio entre las dos palabras.
+
+mascota= input("¿Cómo se llama tu mascota? ")
+ciudad= input("¿En que ciudad vives? ")
+
+print(mascota + " de " + ciudad)
